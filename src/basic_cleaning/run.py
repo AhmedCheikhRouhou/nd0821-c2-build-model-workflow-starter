ff03a14bded3076a0fc022bf38bab93b962f3a6d
Ahmed Cheikh Rouhou <a.cheikhrouhou@instadeep.com>
2022-03-24 15:17:38 +0100

    fix data coverage

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -38,6 +38,10 @@ def go(args):
      type=args.output_type,
      description=args.output_description,
     )
+    
+
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     df.to_csv("clean_sample.csv", index=False)
     artifact.add_file("clean_sample.csv")
